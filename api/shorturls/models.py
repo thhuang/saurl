12cb5f9a719b141890571ffca3ad2df6804cf70b
TH Huang <thhuang@caealumni.ce.ntu.edu.tw>
2019-10-17 09:36:08 +0800

    add has_password field

--- api/shorturls/models.py
+++ api/shorturls/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 
 class ShortUrl(models.Model):
-    user_id = models.CharField(max_length=20)
+    user_id = models.CharField(max_length=40)
     url_code = models.CharField(max_length=20)
     long_url = models.TextField()
     short_url = models.CharField(max_length=40)
     created = models.DateTimeField()
+    has_password = models.BooleanField(default=False)
