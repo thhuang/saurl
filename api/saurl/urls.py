5c1c96ef0bb6677b721378c0f3a92ca608a7d1cd
TH Huang <thhuang@caealumni.ce.ntu.edu.tw>
2019-10-18 15:54:05 +0800

    refactor

--- api/saurl/urls.py
+++ api/saurl/urls.py
@@ -1,8 +1,6 @@
-from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('shorturls/', include('shorturls.urls')),
-    path('admin/', admin.site.urls),
     path('', include('pages.urls')),
 ]
