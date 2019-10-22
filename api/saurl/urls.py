220f21011ba36a8a7fe13bfacb70a086f9a2f50d
TH Huang <thhuang@caealumni.ce.ntu.edu.tw>
2019-10-22 10:14:03 +0800

    update

--- api/saurl/urls.py
+++ api/saurl/urls.py
@@ -1,9 +1,7 @@
-# from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import include, path
 from .router import router
 
 urlpatterns = [
     path('api/', include(router.urls), name='api'),
-    # path('api-token-auth/', obtain_auth_token, name='api-token-auth'),
     path('', include('pages.urls')),
 ]
--- client/src/api/saurl.ts
+++ client/src/api/saurl.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-console.log(process.env.REACT_APP_API_TOKEN)
-
 export default axios.create({
   baseURL: '/api',
   headers: {
